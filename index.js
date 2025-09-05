80baf1731fb901ddca8ba9ba85d8e585161b70e5
Dhruvi Desai <desaidhruvi345@gmail.com>
2025-09-05 21:15:29 +0530

    class-2 (Express)

--- index.js
+++ index.js
@@ -1,18 +1,87 @@
-// INCLUDING EXPRESS module and initialising an app
-const express =require('express');
+// // creating https server with express
 
+const { profile } = require("console");
+
+// // express.
+// // node default laibrary
+
+
+// const express = require("express");
+
+// const app = express();
+
+
+// // function sum (n){
+// //     let ans = 0;
+// //     for(let i =1; i<=n; i++){
+// //         ans = ans +1
+// //     }
+// //     return ans;
+// // }
+
+// app.get("/" , function(req,res){
+//     // const n = req.query.n;
+//     // const ans = sum(n)
+//     // res.send("Hi dhruvii your ans is" + ans)
+// })
+
+
+// app.listen(3000)
+
+
+
+
+
+
+const express = require("express");
 const app = express();
 
-// variable that stores the port number
-const PORT = 3000;
 
-// get 
-app.get('/', (req,res)=>{
-    res.send("hello dhruvi jii")
+ const users  = [{
+    name:"dhruvi",
+    kindney :[{
+        healthy : false
+    }]
+ }];
+
+  app.use(express.json());   
+
+
+ app.get('/', function(req,res) {
+    // write logic here 
+ 
+    const dhruvikidneys = users[0].kindney;
+    const numberOfKindneys = dhruvikidneys.length;
+    let numberOfHealthyKindneys = 0;
+    for (let i=0; i<dhruvikidneys.length; i++){
+            if(dhruvikidneys[i].healthy){
+                numberOfHealthyKindneys = numberOfHealthyKindneys +1
+            }
+    }
+
+    const numberOfUnhealthyKindney = numberOfKindneys - numberOfHealthyKindneys;
+    res.json({
+        numberOfKindneys,
+        numberOfHealthyKindneys,
+        numberOfUnhealthyKindney
+    })
 })
 
-// start the server
 
-app.listen(PORT, ()=>{
-    console.log("application started")
+app.post ('/' , function (req,res){
+    const isHealthy = req.body.isHealthy;
+    users[0].kindney.push({
+        healthy:isHealthy
+    })
+
+    res.json({
+        msg:"Done ! Thank you "
+    })
 })
+
+
+
+
+
+app.listen(3000);
+
